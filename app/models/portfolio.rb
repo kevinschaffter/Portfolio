cb83678049b3c3683795ebd052d19ca751faa80b
Kevin Schaffter <kevinschaffter@gmail.com>
2017-09-17 18:17:11 -0400

    Implemented defaults for Portfolio controller

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,10 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'RoR')}
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
