29d82195fa2e74c9dc16e2e7293d1f526d7f8c0c
Kevin Schaffter <kevinschaffter@Kevins-MacBook-Pro.local>
2017-10-03 20:22:55 -0400

    added collapseable buttons to about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,48 @@ module ApplicationHelper
   def alert_generator msg
       js add_gritter(msg, title: "Kevin Schaffter Portfolio", sticky: false)
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
 end
