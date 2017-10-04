0f4b22b0b40cb7e306550749df3b1048c16b741e
Kevin Schaffter <kevinschaffter@Kevins-MacBook-Pro.local>
2017-10-04 15:51:46 -0400

    integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://kevin-schaffter-portfolio.herokuapp.com', 'http://kevin-schaffter-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://kevin-schaffter-portfolio.herokuapp.com/cable"
 end
