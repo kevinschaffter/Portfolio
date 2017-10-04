e083b5aa1a2e6d6f728d643732a6d05885e9ca14
Kevin Schaffter <kevinschaffter@Kevins-MacBook-Pro.local>
2017-10-04 11:46:15 -0400

    Updated gemfile and declared ruby version

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ gem 'twitter', '~> 6.1'
 gem 'redis', '~> 3.3', '>= 3.3.1'
 gem 'redcarpet', '~> 3.4'
 gem 'coderay', '~> 1.1', '>= 1.1.2'
+
+ruby "2.4.2"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+User.create!(
+  email: "test@test.com",
+  password: "aoeuaoeu",
+  password_confirmation: "aoeuaoeu",
+  name: "Admin User",
+  roles: "site_admin"
+  )
+
+puts "1 Admin user created"
+
 3.times do |topic|
   Topic.create!(
     title: "#{topic}"
